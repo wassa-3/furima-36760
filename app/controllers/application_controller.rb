d5577ddeb1e736964fc3c7d4874fac77f960ea49
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-13 14:37:48 +0900

    ストロングパラメーター

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,5 +8,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["FURIMA_BASIC_USER"] && password == ENV["FURIMA_BASIC_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:date_of_birth])
   end
 end
