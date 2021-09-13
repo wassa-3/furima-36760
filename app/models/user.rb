5242097a8f657fe02f88cc29fd63b1b5635ce277
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-13 18:19:39 +0900

    バリデーション　正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'} do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/, message: 'is invalid. Input full-width katakana characters'} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
   validates :date_of_birth, presence: true
 end
