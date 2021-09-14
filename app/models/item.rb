a743d031ee810fa2f934db7d1a522e5376774f20
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-14 22:29:50 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :pay_by
+  belongs_to :prefecture
+  belongs_to :delivery_time
+
+  with_options presence: true do
+    validates :name
+    validates :explain
+  end
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :pay_by_id
+    validates :prefecture_id
+    validates :delivery_time_id
+  end
+  validates :price, numericality: { in 300..9999999, only_integer: true }
 end
