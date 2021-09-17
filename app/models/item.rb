ebb28beb81349a906a5b13d6957f2e3e391a4e99
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-17 12:08:32 +0900

    モデル エラー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,5 +19,6 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :delivery_time_id
   end
-  validates :price, numericality: { in 300..9999999, only_integer: true }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+  validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
 end
--- app/models/pay_by.rb
+++ app/models/pay_by.rb
@@ -1,4 +1,4 @@
-class Pay_by < ActiveHash::Base
+class PayBy < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '着払い(購入者負担)' },
