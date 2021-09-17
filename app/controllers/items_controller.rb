c0c98519e830701debe87bfaf0ff920f5cef88bf
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-17 12:07:03 +0900

    createアクション　コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,19 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :explain, :category_id, :condition_id, :pay_by_id, :prefecture_id, :delivery_time_id, :price).merge(user_id: current_user.id)
+  end
 end
