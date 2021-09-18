b498f45f6b60302a66415aa79b87ee35d00a602f
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-18 21:26:28 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: [:edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
