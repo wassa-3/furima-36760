8839d60155c74216a83cc4610cb37ca2043b402a
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-20 14:56:38 +0900

    バリデーション　記述ミス修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,11 +8,12 @@ class OrderAddress
     validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :address_line
     validates :address_number
-    validates :phone_number: {only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 11, message: 'is invalid'}
+    validates :phone_number, numericality: {only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 11, message: 'is invalid'}
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(zip: zip, prefecture_id: prefecture_id, address_line: address_line, address_number: address_number, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
