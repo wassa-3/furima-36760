b2ef03fbc73b2d97f67d89327eac5eccbb27808a
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-20 15:31:22 +0900

    order、address　マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,16 @@ ActiveRecord::Schema.define(version: 2021_09_20_031836) do
   end
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "zip", null: false
+    t.integer "prefecture_id", null: false
+    t.string "address_line", null: false
+    t.string "address_number", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_addresses_on_order_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -54,8 +62,12 @@ ActiveRecord::Schema.define(version: 2021_09_20_031836) do
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
