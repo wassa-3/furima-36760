6467b81176c5ae01245b81bcff020cafa986643c
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-20 15:55:44 +0900

    ordersコントローラー create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,18 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new
   end
   def create
-    
+    @order_address = OrderAddress.new(order_params)
+    if @order_address,valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+    def order_params
+      params.require(:order_address).permit(:zip, :prefecture_id, :address_line, :address_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    end
 end
