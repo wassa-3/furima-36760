4e889f9ede099715562e617bc5f61b4c8ecba596
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-20 16:46:01 +0900

    orderコントローラー create修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,9 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new
   end
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-    if @order_address,valid?
+    if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
