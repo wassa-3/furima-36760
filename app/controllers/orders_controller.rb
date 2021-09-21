160323495ad5aae719f5fdd35503e5a54d284a1c
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-21 09:33:03 +0900

    API関連　設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -18,5 +19,14 @@ class OrdersController < ApplicationController
 
     def order_params
       params.require(:order_address).permit(:zip, :prefecture_id, :address_line, :address_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+    end
+
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
     end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,6 +9,7 @@ class OrderAddress
     validates :address_line
     validates :address_number
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: 'is invalid'}
+    validates :token
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
