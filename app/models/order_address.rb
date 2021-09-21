bd1e30cebc60cf2ca19d5dc6ec357b7e0b0c80d5
wassa-3 <89175437+wassa-3@users.noreply.github.com>
2021-09-21 12:48:44 +0900

    ビューファイル　soldout 条件分岐

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,13 +5,13 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :address_line
     validates :address_number
-    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: 'is invalid'}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Input only number. Phone number is too short"}
     validates :token
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
